e4a103e4998d3effa647fab04271b28a16a4852a
Matthieu Lemerre <matthieu.lemerre@gmail.com>
2012-10-13 11:20:16 +0200

    New CPS Variable abstraction

---
+++ src/cps/cpsvar.ml
@@ -0,0 +1,218 @@
+(* Copyright 2012 Matthieu Lemerre *)
+
+(*s This file implements links between variables, following the
+  "Compiling with continuations, continued" paper by Andrew Kennedy,
+  and "Shrinking lambda expressions in linear time", by Andrew Appel
+  and Trevor Jim.
+
+  Occurrences are linked to one another into a doubly-linked list,
+  such that the doubly-linked list represents the set of all
+  occurrences of a variable. A variable is linked to one of its
+  occurrence (and thus to the set of all occurrences of the
+  variable). This allows adding occurrence, removing occurence, and
+  knowing if there is 0,1, or more occurrence to a variable, in O(1)
+  time.
+
+  The set of all occurrences of all variables is partitionned into
+  same-binder equivalence classes using a union-find data structure,
+  i.e. all the members of a partition are occurrences of the same
+  variable. The description of the partition is the binding variable
+  itself. This allows near-constant time access to the binding
+  variable, and merging of occurrence sets. *)
+
+module Union_find = Union_find.Safe;;
+
+(*s Variables link to their description, and to one occurrence (if
+  there is one). We also add an integer id and the number of integers
+  created so far, but only to help in debugging. *)
+type ('a,'b) variable =
+    { mutable var_desc: 'a initialized;
+      mutable occurrences:('a, 'b) occurrence option;
+      var_id: display_id;
+      mutable occur_nb: display_id }
+
+(* Occurrences need a description; a "union-find link" (to retrieve
+   the binding variable, which is the description of the union-find
+   partition); and the links of the doubly-linked lists. *)
+and ('a,'b) occurrence = 
+    { mutable occur_desc: 'b initialized;
+      mutable link_var: (('a, 'b) variable,('a, 'b) occurrence) Union_find.link;
+      mutable next_occurrence:('a, 'b) occurrence;
+      mutable previous_occurrence:('a, 'b) occurrence;
+      occur_id: display_id }
+
+(* We have to check that the user does not retrieve the description
+   until the variable is initialized, or that it does not overwrite an
+   already initialized description. *)
+and 'a initialized = Undef | Initialized_to of 'a
+
+(* [display_id] integers are used just as a way of displaying
+   understandable debugging infos, and can be safely removed in
+   non-debug versions. *)
+and display_id = int;;
+
+
+
+
+(*s The CPSVar functor takes a DESCRIPTION argument as explained in the interface.  *)
+module type DESCRIPTION = sig
+  type variable_description
+  type occurrence_description
+  val var_prefix:string
+end 
+
+module Make(Desc:DESCRIPTION) = struct
+
+  type var = (Desc.variable_description, Desc.occurrence_description) variable
+  type occur = (Desc.variable_description, Desc.occurrence_description) occurrence
+
+  (*s Occurrences contain a link variable, used by the union-find
+    functor. The description of a partition of an occurrence [occur]
+    is the variable [var] for which [occur] is an occurrence. *)
+  module Var_union_find = Union_find.Make(struct
+    type description = var
+    type element = occur
+    let set f v = f.link_var <- v
+    let get f = f.link_var
+  end)
+
+  (* The Union-find module requires a "union-find data structure"
+     (ufds) as its first argument. Ideally, we should have one such
+     ufds per CPS term, as a variable and all its occurrences cannot
+     be shared between several terms. However, using a single ufds for
+     all terms in a global variable is more practical (no need to pass
+     an additional argument). Moreover, confusion between partitions
+     of several terms should be relatively easy to avoid, so checking
+     that with several ufds is not absolutely necessary. *)
+  let ufds = Var_union_find.create();;
+
+  module Var = struct
+
+    (*s The tryptic make/description/set_description allows building
+      circular structures without simultaneous definitions.
+      [var_counter] provides unique identifiers when displaying
+      variables. *)
+
+    let var_counter:display_id ref = ref 0;;
+
+    let make() = var_counter := !var_counter + 1;
+      { var_desc = Undef;
+        occurrences = None;
+        var_id = !var_counter;
+        occur_nb = 0 } ;;
+
+    let description b = match b.var_desc with
+      | Initialized_to(d) -> d
+      | Undef -> assert false (*r Trying to access an uninitialized description. *)
+
+    let set_description b d = match b.var_desc with
+      | Initialized_to(d) -> assert false   (*r Description already initialized. *) 
+      | Undef -> b.var_desc <- Initialized_to(d) 
+
+    (*s Occurrence number can return in O(1) whether a variable has
+      no, one, or more than one occurrence. For that we must not
+      return the list of occurrence if there are several; instead the
+      user must then use the [fold_on_occurrences] function. *)
+    type occurrence_number = 
+      | No_occurrence
+      | One_occurrence of occur
+      | Several_occurrences
+
+    let occurrence_number var = 
+      match var.occurrences with
+        | None -> No_occurrence
+        | Some(occ) -> 
+          if occ.previous_occurrence == occ
+          then begin
+            assert (occ.next_occurrence == occ);
+            One_occurrence occ
+          end
+          else Several_occurrences;;
+
+    let fold_on_occurrences var init f = 
+      match var.occurrences with
+        | None -> init
+        | Some(occ) -> 
+          let rec loop value cur = 
+            if cur == occ
+            then f value cur
+            else loop (f value cur) cur.next_occurrence
+          in loop init occ.next_occurrence;;
+
+    (*s Utility functions. *)
+    let to_string bv = Desc.var_prefix ^ (string_of_int bv.var_id);;
+
+    module Map = Map.Make(struct
+      type t = var
+      let compare = compare
+    end)
+
+    module Set = Set.Make(struct
+      type t = var
+      let compare = compare
+    end)
+
+  end;;
+
+  module Occur = 
+  struct
+
+    (*s Create a new occurrence of VAR, i.e. add it to the
+      doubly-linked list of all the occurrences of VAR, and to the
+      union-find partition of the occurrences of VAR. *)
+    let make var = 
+      (* Create an initial self-linked occurrence of var.  *)
+      let self_linked_occur var = 
+        var.occur_nb <- var.occur_nb + 1;
+        let rec occur = { occur_desc = Undef;
+                          link_var = Union_find.empty_link;
+                          next_occurrence = occur;
+                          previous_occurrence = occur;
+                          occur_id = var.occur_nb; } 
+        in occur
+      in
+      match var.occurrences with
+        (* If this is the first occurrence of var. *)
+        | None -> 
+          let new_occur = self_linked_occur var in
+          Var_union_find.singleton ufds new_occur var;
+          var.occurrences <- Some(new_occur);
+          new_occur
+        (* Else there are already some occurrences of var. *)
+        | Some(existing_occur) -> 
+          let new_occur = self_linked_occur var in
+          (* Insert new between existing and existing.next. *)
+          let following_occur = existing_occur.next_occurrence in
+          let new_occur = { new_occur
+                            with next_occurrence = following_occur;
+                              previous_occurrence = existing_occur } in
+          existing_occur.next_occurrence <- new_occur;
+          following_occur.previous_occurrence <- new_occur;
+          (* Merge new to the partition of occurrences of var. *)
+          let new_part = Var_union_find.singleton ufds new_occur var in
+          let existing_part = (Var_union_find.find ufds existing_occur) in
+          Var_union_find.union ufds new_part existing_part var;
+          new_occur;;
+
+    (* Returns the variable of which [occur] is an occurrence. *)
+    let binding_variable occur =
+      let part = Var_union_find.find ufds occur in
+      Var_union_find.description ufds part;;
+
+    (* Similarly to [Var], description can be get and set only once.  *)
+    let description b = match b.occur_desc with
+      | Initialized_to(d) -> d
+      | Undef -> assert false (*r Trying to access an uninitialized description. *)
+
+    let set_description b d = match b.occur_desc with
+      | Initialized_to(d) -> assert false   (*r Description already initialized. *)
+      | Undef -> b.occur_desc <- Initialized_to(d) 
+
+    (* We suffix the name of the variable by the occurrence number,
+       which may be useful to follow code transformations steps. *)
+    let to_string occur = 
+      let bv = binding_variable occur in
+      Desc.var_prefix ^ (string_of_int bv.var_id) ^ "_" ^ (string_of_int occur.occur_id);;
+  end
+
+end
---
+++ src/cps/cpsvar.mli
@@ -0,0 +1,82 @@
+(* Copyright 2012 Matthieu Lemerre *)
+
+(* There are two structures where information about variables is
+   attached. [occurrence] stores data about the use of a variable
+   (like the location of the use). [variable] stores data common to
+   all the uses of the variable, like its type, or the location where
+   the variable is bound. *)
+type ('a, 'b) variable
+type ('a, 'b) occurrence
+
+(* The user of the module must configure the information it wants to
+   attach to a [variable] or [occurrence] (using the DESC structure).
+   This information is called a [description].
+
+   The [make] functions, that create new variables and occurence, do
+   not take an initial [description] argument. This allows to break
+   circular dependencies, e.g. where a term points to a variable or
+   occurrence, and a variable points to the term that bind or use it.
+
+   Instead, for both the variable and its occurrence, the description
+   is set with the [set_description] function, and obtained with the
+   [description] function. It is illegal (and checked by assertions)
+   to set a description more than once, or to call [description] on a
+   variable when the description has not been set. 
+
+   The [var_prefix] parameter is used as a prefix to the [to_string]
+   function. For exemple if prefix is "x", [to_string] may return
+   "x10" for a variable and "x10_2" for one of its occurrence. *)
+module type DESCRIPTION = sig 
+  type variable_description
+  type occurrence_description
+  val var_prefix:string
+end
+module Make (Desc : DESCRIPTION) :
+sig
+  type var = (Desc.variable_description, Desc.occurrence_description) variable
+  type occur = (Desc.variable_description, Desc.occurrence_description) occurrence
+
+  module Var :
+  sig
+
+    (* [make ()] create a new variable. *)
+    val make : unit -> var
+
+    (* The following function allows to differentiate, in O(1) time,
+       between the 0-occurrence case (the variable is useless), the
+       1-occurrence case (a good candidate for inlining), and the case
+       with more than one occurrence. *)
+    type occurrence_number = 
+      | No_occurrence
+      | One_occurrence of occur
+      | Several_occurrences
+    val occurrence_number: var -> occurrence_number
+
+    (* This function allows to iterate on occurrences. The order is
+       arbitrary. *)
+    val fold_on_occurrences: var -> 'a -> ('a -> occur -> 'a) -> 'a
+
+    val description : var -> Desc.variable_description
+    val set_description : var -> Desc.variable_description -> unit
+    val to_string : var -> string
+
+    module Map : Map.S with type key = var
+    module Set : Set.S with type elt = var
+  end
+
+  module Occur :
+  sig
+
+    (* [make var] creates a new occurrence of [var]. *)
+    val make : var -> occur
+
+    (* [binding_variable occur] returns the binding variable of
+       [occur], i.e. the variable [var] such that [occur] is an
+       occurrence of [var]. *)
+    val binding_variable : occur -> var
+
+    val description: occur -> Desc.occurrence_description
+    val set_description: occur -> Desc.occurrence_description -> unit
+    val to_string : occur -> string
+  end
+end
