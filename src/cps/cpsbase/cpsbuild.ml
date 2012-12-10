d469cb5f8bf2c7aa65a3a88312e01d3d077b8f79
Matthieu Lemerre <matthieu.lemerre@gmail.com>
2012-12-10 02:07:12 +0100

    Cpsbuild: new module

---
+++ src/cps/cpsbase/cpsbuild.ml
@@ -0,0 +1,204 @@
+(* Copyright 2012 Matthieu Lemerre *)
+
+(*s This module provides means to build CPS terms, using an
+  easy-to-use higher-order abstract syntax API.
+
+  In particular, this module deals with the problem of mutual
+  recursion caused by the presence of uplinks in some elements (from
+  the elements to their enclosing terms).
+
+  \paragraph{The [~reconnect] argument}
+
+  The building functions take an additional [~reconnect] argument of
+  type [term]; the purpose of this argument is to be replaced by the
+  building functions while preserving the up- and down-links.
+
+  When performing a replacement of a term [t] of type [term], we keep
+  the [term] structure and change only [t.term] (of type [term_]).
+  This preserves the downlinks from enclosing terms, and the uplink
+  from [t] to its enclosing term.
+
+  But we must take care that any element (term or variable) that [t]
+  encloses points appropriately to [t]. We could first create a new
+  [term_] [t'], then change [t.term] to [t'], and then change the
+  uplinks of elements in [t'] to point to [t]. Changing the uplinks
+  is an O(1) operation, but not very efficient, that can be
+  completely avoided if the uplinks to [t] are set directly when
+  building the term [t']. This is why the functions in the [Build]
+  module provide the [~reconnect] argument. *)
+open Cpsdef;;
+
+(*s We create a [Make] functor factorizes the code for variables and
+  continuation variables. It provides a function, [with_var], that
+  allows creating new variables; [with_var] is used by the [Build]
+  functions, but can also be called by the user.
+
+  In addition to the [Var] or [Cont_var] module, and to [set_uplink],
+  a function to change the uplink of a variable, the functor takes as
+  an argument [var_in_term]. The [with_var] function takes as
+  argument a function [f], that when called, returns a new term [t];
+  the variable created by [with_var] is supposed to be bound by [t]
+  (and not by the subterms of [t]). [var_in_term] allows to check
+  that. *)
+module type VAR_PARAM = sig
+  module Var : Cpsdef.VAR
+  val var_in_term: Var.var -> Cpsdef.term -> bool
+  val set_uplink: Var.var -> Cpsdef.term -> unit
+end
+
+module Var_param = struct
+  module Var = Cpsdef.Var
+  let var_in_term v t = Cpscheck.Contains.var (Term.get t) v;;
+  let set_uplink v t =
+    (* Note: set_description checks that var does not yet have a
+       description, i.e. that the variable is fresh. *)
+    assert ( try ( Var.Var.binding_site v); false with Assert_failure _ -> true);
+    Var.Var.init v (Enclosing_term t);;
+end;;
+
+module Cont_var_param = struct
+  module Var = Cpsdef.Cont_var
+  let var_in_term v t = Cpscheck.Contains.cont_var (Term.get t) v;;
+  let set_uplink v t = ();;
+end
+
+
+module Make(M:VAR_PARAM) = struct
+  (* [with_var] creates a new var, and pass it to [f]. [f] returns a
+     term, and [with_var] creates an uplink from the new var to that
+     term. The [?var] optional argument allows to reuse an existing
+     variable, instead of creating a new one; this variable must not
+     be bound elsewhere (and has been created using another call to
+     [with_var]). *)
+  let with_var ?var f =  match var with
+
+    (* We create the variable, then update it so that it is bound to
+       the term used to created it. *)
+    | None ->
+      let v = M.Var.Var.make() in
+      let t = f v in
+      assert( M.var_in_term v t);
+      M.set_uplink v t;
+      t
+
+    (* If a var argument was given, then the argument was created
+       using an explicit [with_var]. So there is no need to set the
+       uplink; that call [with_var] will do it. *)
+    | Some(x) -> let t = f x in
+                 assert( M.var_in_term x t); t
+end
+
+module With_var = Make(Var_param)
+let with_var = With_var.with_var;;
+
+module With_cont_var = Make(Cont_var_param)
+let with_cont_var = With_cont_var.with_var;;
+
+(* Similarly to the [with_var] functions, [with_subterm] is used to
+   update a fresh term.  *)
+let with_subterm fresh f =
+  let t = f fresh in
+  assert (Cpscheck.Contains.subterm (Term.get t) fresh);
+  Cpscheck.And_set.enclosing fresh (Enclosing_term t);
+  t;;
+
+(*s The term building functions are straightforward. Note that we do
+  not expose the primitive functions, like [let_prim] or [let_value],
+  in the interface, and prefer exposing more specific functions (such
+  as [let_constant] or [let_proj]). This makes translation to CPS more
+  agnostic to changes in the [Cpsdef.term_] data structure. *)
+
+let let_prim ?reconnect ?var prim fterm =
+  with_var ?var (fun v ->
+    with_subterm (fterm v) (fun body ->
+      Term.make ?reconnect (Let_prim(v,prim,body))))
+
+let let_value ?reconnect ?var value fterm =
+  let_prim ?reconnect ?var (Value value) fterm
+
+let let_proj ?reconnect ?var to_proj i fterm =
+  let_prim ?reconnect ?var (Projection(Var.Occur.make to_proj, i)) fterm
+
+let let_pair ?reconnect ?var (x0,x1) fterm =
+  let (o0,o1) = (Var.Occur.make x0, Var.Occur.make x1) in
+  let_value ?reconnect ?var (Tuple [o0;o1]) fterm;;
+
+(* Note: contrary to the [Ast] level, we do allow tuples with 0 or 1
+   element. 0-element tuples correspond to "void" or "undef values"
+   (they are pointers that should not be dereferenced), and 1-element
+   tuples to simple references (pointers to a single element). *)
+let let_tuple ?reconnect ?var l_vars fterm =
+  let l_occurs = List.map Var.Occur.make l_vars in
+  let_value ?reconnect ?var (Tuple l_occurs) fterm;;
+
+let let_void ?reconnect ?var fterm =
+  let_value ?reconnect ?var Void fterm
+
+let match_pair ?reconnect ?var0 ?var1 x fterm =
+  let_proj ?reconnect ?var:var0 x 0 (fun v0 ->
+    let_proj ?var:var1 x 1 (fun v1 ->
+      fterm (v0, v1)))
+
+let match_tuple ?reconnect n x fterm =
+  let rec f k vars =
+    if k == n then (fterm vars)
+    else let_proj x (n-1-k) ( fun var ->
+      f (k+1) (var::vars) )
+  in
+
+  if reconnect != None
+  (* We use [let_void] just to reconnect, in particular for the
+     special case n == 0. The additional term can be removed by later
+     cleanup passes, or in the machine code translation. *)
+  then let_void ?reconnect (fun _ -> f 0 [])
+  else f 0 [];;
+
+let let_constant ?reconnect ?var c fterm =
+  let_value ?reconnect ?var (Constant c) fterm
+
+let let_binary_op ?reconnect ?var op a b fterm =
+  let (oa, ob) = (Var.Occur.make a, Var.Occur.make b) in
+  let_prim ?reconnect ?var (Integer_binary_op(op,oa,ob)) fterm
+
+let let_lambda ?reconnect ?lambda_var ?param_var ftermlambda ftermparam =
+  with_cont_var ( fun cv ->
+    with_var ?var:lambda_var ( fun lambdav ->
+      with_var ?var:param_var ( fun paramv ->
+        with_subterm (ftermparam lambdav) (fun body_param ->
+          with_subterm (ftermlambda (cv,paramv)) (fun body_lambda ->
+            Term.make ?reconnect
+              (Let_prim(lambdav,
+                        Value (Lambda(cv,paramv, body_lambda)),
+                        body_param)))))));;
+
+let apply ?reconnect f k x =
+  Term.make ?reconnect
+    (Apply ((Var.Occur.make f),
+            (Cont_var.Occur.make k),
+            (Var.Occur.make x)));;
+
+let let_cont ?reconnect fterm1 fterm2 =
+  with_cont_var ( fun cv ->
+    with_var( fun v ->
+      with_subterm (fterm1 v) (fun t1 ->
+        with_subterm (fterm2 cv) (fun t2 ->
+          Term.make ?reconnect (Let_cont(cv, v, t1, t2 ))))));;
+
+let apply_cont ?reconnect k x =
+  Term.make ?reconnect
+    (Apply_cont ((Cont_var.Occur.make k),
+                 (Var.Occur.make x)));;
+
+let halt ?reconnect x =
+  Term.make ?reconnect (Halt (Var.Occur.make x));;
+
+(*s Functions outside of [Build] do not need the [?var] optional
+  argument, so we hide it. *)
+let with_var_in_term f = with_var f;;
+let with_cont_var_in_term f = with_cont_var f;;
+
+let with_var_in_def f =
+  let v = Cpsdef.Var.Var.make() in
+  let d = f v in
+  Cpsdef.Var.Var.init v (Cpsdef.Enclosing_definition d);
+  d;;
---
+++ src/cps/cpsbase/cpsbuild.mli
@@ -0,0 +1,158 @@
+(* Copyright 2012 Matthieu Lemerre *)
+
+open Cpsdef;;
+
+(* The functions of this [Build] module are the only provided mean to
+   build [term]s.
+
+   These functions must be used following the higher-order abstract
+   syntax style, i.e. using OCaml bindings to represent CPS bindings.
+   For instance, the term
+
+   [ let x = 11 in
+   halt x ]
+
+   Is built using
+
+   [ Build.let_constant (Constant.Int 11) (fun x ->
+   Build.halt x) ]
+
+   This API simplifies the creation of bindings (by avoiding the need to
+   explicitly call functions to create bindings, like Lisp's
+   [gensym]), and provides some guarantee that the binding cannot
+   escape its scope (i.e. in the above [x] can be used only in
+   subterms of [let_constant]).
+
+   However, instead of automatically creating new variables, the build
+   functions can also use an existing variable, using the [~var] named
+   parameter. New variables and continuation variables can also be
+   explicitely created using [with_var] and [with_cont_var]. Thus the
+   above exemple can be rewritten as:
+
+   [ with_var (fun x ->
+   Build.let_constant ~var:x (Constant.Int 11) (fun x' ->
+   Build.halt x)) ]
+
+   And we have [x == x']. This is especially useful when in need to
+   act on the occurrences of a newly created variable.
+
+   By default, the functions of the [Build] module create a new, fresh
+   term. However, they can also be used to replace the contents of an
+   existing term [t]: first [t] needs to be disconnected using the
+   [Change] module; then it can be reconnected using the [~reconnect]
+   argument. For instance, the previous exemple can be changed like
+   the following:
+
+   [let Let_prim(_,_,body) = Term.get t in
+   let (dangling_body, body_to_reuse) = Change.disconnect body in
+   ignore(
+   Build.let_constant ~reconnect:dangling_body (Constant.Int 12) (fun y ->
+   body_to_reuse))]
+
+   which has transformed t to:
+
+   [let x = 11 in
+   let y = 12 in
+   halt x]
+
+   Note: the API automatically handles creation of occurrences of
+   variables. It is not possible to "re-use" an occurrence of a
+   variable, or pass an occurrence to [Build] functions; you should
+   delete the occurrence and let [Build] re-create it instead. *)
+
+
+(* The functions used to explicitely create new variables. *)
+val with_var_in_term : (var -> Fresh.t) -> Fresh.t
+val with_cont_var_in_term : (cont_var -> Fresh.t) -> Fresh.t
+val with_var_in_def : (var -> Cpsdef.definition) -> Cpsdef.definition
+
+(* Usage: [let_constant c (fun var -> ...)] corresponds to
+   [let var = c in ...] *)
+val let_constant :
+  ?reconnect:Empty.t ->
+  ?var:var ->
+  Constant.t -> (var -> Fresh.t) -> Fresh.t
+
+(* Usage: [let_void (fun var -> ...)] corresponds to
+   [let var = void in ...] *)
+val let_void :
+  ?reconnect:Empty.t ->
+  ?var:var -> (var -> Fresh.t) -> Fresh.t
+
+(* Usage: [let_proj tuple i (fun var -> ...) corresponds to
+   [let var = #i(tuple) in ... ] *)
+val let_proj :
+  ?reconnect:Empty.t ->
+  ?var:var ->
+  var -> int -> (var -> Fresh.t) -> Fresh.t
+
+(* Usage: [match_pair pair (fun (var0,var1) -> ...)] corresponds to
+   [let (var0,var1) = pair in ...], i.e. to
+   [let var0 = #0(pair) in
+    let var1 = #1(pair) in ... ] *)
+val match_pair :
+  ?reconnect:Empty.t ->
+  ?var0:var ->
+  ?var1:var ->
+  var -> (var * var -> Fresh.t) -> Fresh.t
+
+(* Usage: [let_pair (var0,var1) (fun var -> ...)] corresponds to
+   [let var = (var0,var1) in ...] *)
+val let_pair :
+  ?reconnect:Empty.t ->
+  ?var:var ->
+  var * var -> (var -> Fresh.t) -> Fresh.t
+
+(* Usage: [let_tuple [var0;var1;...] (fun var -> ...)] corresponds to
+   [let var = (var0,var1,...) in ...]. Note that 0- and 1-tuples are
+   allowed. *)
+val let_tuple :
+  ?reconnect:Empty.t ->
+  ?var:var ->
+  var list -> (var -> Fresh.t) -> Fresh.t
+
+(* Usage: [match_tuple t (fun vars -> ...)] corresponds to
+   [let [var0;var1;...] = t in ...], i.e. to
+   [let var0 = #0(t) in
+    let var1 = #1(t) in ... ] *)
+val match_tuple :
+  ?reconnect:Empty.t ->
+  int -> var -> (var list -> Fresh.t) -> Fresh.t
+
+(* Usage: [let_binary_op op a b (fun var -> ...)] corresponds to
+   [let var = a op b in ...] *)
+val let_binary_op :
+  ?reconnect:Empty.t ->
+  ?var:var ->
+  Constant.integer_binary_op -> var -> var -> (var -> Fresh.t) -> Fresh.t
+
+(* Usage: [let_lambda (fun (k,x) -> ...) (fun var -> ... )] corresponds to
+   [let var = { (k,x) -> ... } in ...] *)
+val let_lambda :
+  ?reconnect:Empty.t ->
+  ?lambda_var:var ->
+  ?param_var:var ->
+  (cont_var * var -> Fresh.t) -> (var -> Fresh.t) -> Fresh.t
+
+(* Usage: [apply f k x] corresponds to f(k,x) *)
+val apply :
+  ?reconnect:Empty.t ->
+  var -> cont_var -> var -> Fresh.t
+
+(* Usage: [let_cont (fun x -> ...) (fun k -> ...)] corresponds to
+   [do ... where k(x) = ...]
+   TODO: The first argument of [let_cont] should also receive [k], so
+   as to allow loops. *)
+val let_cont :
+  ?reconnect:Empty.t ->
+  (var -> Fresh.t) -> (cont_var -> Fresh.t) -> Fresh.t
+
+(* Usage: [apply_cont k x] corresponds to k(x) *)
+val apply_cont :
+  ?reconnect:Empty.t ->
+  cont_var -> var -> Fresh.t
+
+(* Usage: [halt x] corresponds to [halt x]. *)
+val halt :
+  ?reconnect:Empty.t ->
+  var -> Fresh.t
