d28985c2d4f544191a8ac4316e7b2cd1077e2863
Matthieu Lemerre <matthieu.lemerre@gmail.com>
2012-12-27 11:10:43 +0100

    Cps: new module

---
+++ src/cps.ml
@@ -0,0 +1,6 @@
+(* Copyright 2012 Matthieu Lemerre *)
+include Cpspack.Base
+include Cpspack
+
+type 'a var_map = 'a Base.Var.Var.Map.t;;
+module Var_Map = Base.Var.Var.Map;;
---
+++ src/cps.mli
@@ -0,0 +1,15 @@
+(*s Copyright 2012 Matthieu Lemerre *)
+
+(* We just make the main CPS definitions more accessible. *)
+
+module Base:module type of Cpspack.Base;;
+
+type term = Base.term;;
+type toplevel = Base.toplevel;;
+type var = Base.var;;
+type cont_var = Base.cont_var;;
+
+module Var_Map: Map.S with type 'a t = 'a Base.Var.Var.Map.t
+                      and type key = var;;
+
+module Print:module type of Base.Print;;
