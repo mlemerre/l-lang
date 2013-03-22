15960b72f5e1215b1c12ffab90381907961aeb6b
Matthieu Lemerre <matthieu.lemerre@gmail.com>
2013-03-22 14:48:39 +0100

    Log: add a "log level" concept; also throw errors

--- src/support/log.ml
+++ src/support/log.ml
@@ -1,42 +1,74 @@
 (* Copyright 2013 Matthieu Lemerre *)
 
-let output_ (bool,name) x =
+let koutput_ k (bool,name) x =
   if bool
   then
     begin
       Format.printf "#LOG (%s): %! " name;
       (* We use kfprintf to preserve the type of output_ *)
-      Format.kfprintf (fun fmt -> Format.printf "@.") Format.std_formatter x
+      Format.kfprintf (fun fmt -> Format.printf "@."; k()) Format.std_formatter x
     end
   else Format.ifprintf Format.std_formatter x;;
 
+let output_ (bool,name) x = koutput_ (fun () -> ()) (bool,name) x;;
+
 let tmp x = output_ (true, "###tmp###") x;;
 
+type level =
+| Debug
+| Info
+| Warning
+| Error
+| Compiler_error;;
+
+(* Comparison between levels. *)
+let ( >== ) output reference =
+  let to_int = function
+    | Debug -> 0 | Info -> 1 | Warning -> 2 | Error -> 3 | Compiler_error -> 4 in
+  (to_int output) >= (to_int reference)
+
 module type CATEGORY = sig
-  val is_output: bool
-  val output: ('a, Format.formatter, unit) format -> 'a
+  val is_output: level -> bool
+  val output: level -> ('a, Format.formatter, unit) format -> 'a
+
+  val debug: ('a, Format.formatter, unit) format -> 'a
+  val info: ('a, Format.formatter, unit) format -> 'a
+  val warning: ('a, Format.formatter, unit) format -> 'a
+  val raise_user_error: ('a, Format.formatter, unit) format -> 'a
+  val raise_compiler_error: ('a, Format.formatter, unit) format -> 'a
+
+  exception User_error;;
+  exception Compiler_error;;
 end
 
 module type S = sig
-  val is_output: bool
+  val min_output_level: level
   val printed_name: string
 end
 
 module Make(Param:S):CATEGORY = struct
-  include Param;;
-  let output x = output_ (Param.is_output,Param.printed_name) x;;
+  let is_output l = l >== Param.min_output_level;;
+  let output level x = output_ (is_output level,Param.printed_name) x;;
+  let koutput k level x = koutput_ k (is_output level,Param.printed_name) x;;
+
+  let debug x = output Debug x
+  let info x = output Info x
+  let warning x = output Warning x
+
+  exception User_error;;
+  exception Compiler_error;;
+
+  let raise_user_error x = koutput (fun () -> raise User_error) Error x
+  let raise_compiler_error x = koutput (fun () -> raise Compiler_error) Error x
 end
 
-(* For now, set to true/false the first field according to whether you
-   want to see some output. *)
-
+(* For now, we set manually the min output level. *)
 module Free_variables = Make(struct
-  let is_output = true;;
+  let min_output_level = Debug;;
   let printed_name = "free variables"
 end)
 
 module Llvm_output = Make(struct
-  let is_output = false;;
+  let min_output_level = Debug;;
   let printed_name = "llvm output"
 end)
-
--- src/support/log.mli
+++ src/support/log.mli
@@ -1,11 +1,38 @@
 (* Copyright 2012 Matthieu Lemerre. *)
 
-(* Output functions, by category that can be selectively made silent.
-   Each output function has the same type than [Format.printf]. *)
+(* Log and error reporting functions, by category. Logging functions
+   have the same type than [Format.printf]. *)
 
 (* This allows to log something quickly; useful for temporary
    debugging. *)
 val tmp: ('a, Format.formatter, unit) format -> 'a
+
+(* For each category one can choose the severity- (or log-) level, of the
+   error. The levels are:
+
+   \begin{itemize}
+   \item [Debug]: outputs information most useful for the devopper of a
+   module of the compiler. There is no error.
+   \item [Info]: outputs information useful for the user,
+   like opportunities for optimizations. This is not an error, but the
+   user may take action.
+   \item [Warning]: indicates a possible error in the program, but
+   the compiler can still proceed. The user should take action.
+   \item [Error] an error that hinders production of the code of the
+   module.  The user must take action.
+   \item [Compiler_error] indicates that an internal invariant in the
+   compiler was broken; the compiler must be fixed.
+   \end{itemize}
+
+   The last two levels stops the current processing and throw
+   [User_error] and [Compiler_error] exceptions. They can be caught to
+   try to execute the compiler further despite the error. *)
+type level =
+| Debug
+| Info
+| Warning
+| Error
+| Compiler_error;;
 
 (* The [Log] module defines a set of debug options. If the option is
    set, then [Option.is_output] will return true; and [Option.output]
@@ -15,9 +42,28 @@ val tmp: ('a, Format.formatter, unit) format -> 'a
    used.  [Option.is_output] would not be necessary in a lazy
    language. *)
 module type CATEGORY = sig
-  val is_output: bool
-  val output: ('a, Format.formatter, unit) format -> 'a
+
+  (* Is a message output at this level. Useful to avoid extra
+     processing that help display more precise error messages. *)
+  val is_output: level -> bool
+
+  (* Output a message at a given level. Does NOT raise any error. *)
+  val output: level -> ('a, Format.formatter, unit) format -> 'a
+
+  (* [Format.printf]-like functions for each log level. In addition
+     the last two levels throw an error. *)
+  val debug: ('a, Format.formatter, unit) format -> 'a
+  val info: ('a, Format.formatter, unit) format -> 'a
+  val warning: ('a, Format.formatter, unit) format -> 'a
+  val raise_user_error: ('a, Format.formatter, unit) format -> 'a
+  val raise_compiler_error: ('a, Format.formatter, unit) format -> 'a
+  exception User_error;;
+  exception Compiler_error;;
 end
 
+(* TODO: Should we declare the modules here, but in their respective
+   locations? The advantage of declaring it here is that it is a
+   central location to customize the logging. But logging levels
+   should be chosen by the command line/environment anyway. *)
 module Free_variables:CATEGORY;;
 module Llvm_output:CATEGORY;;
